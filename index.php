baed8cc434e615b189f65e7984791abbcc77b34a
Yee Yong Khang IT <s10177247@connect.np.edu.sg>
2021-01-05 14:48:11 +0800

    Init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+// Detect the current session
+session_start();
+
+$MainContent = "<img src='images/welcome2mamaya.jpg'  
+                     class='img-fluid' 
+                     style='display:block; margin:auto;'/>";
+
+include("MasterTemplate.php"); 
+?>
